153c632b8f26182eb4afcf2bbb82caece152cd26
chaima <chaimatmar2023@gmail.com>
2026-01-07 13:59:14 +0100

    Final version with direct structure

--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -7,22 +7,22 @@ const todoSlice = createSlice({
     filter: 'all' 
   },
   reducers: {
-    // إضافة مهمة جديدة
+
     addTask: (state, action) => {
       state.tasks.push(action.payload);
     },
-    // تبديل حالة المهمة (Done/Not Done)
+
     toggleTask: (state, action) => {
       const task = state.tasks.find(t => t.id === action.payload);
       if (task) task.isDone = !task.isDone;
     },
-    // تعديل وصف المهمة
+
     editTask: (state, action) => {
       const { id, description } = action.payload;
       const task = state.tasks.find(t => t.id === id);
       if (task) task.description = description;
     },
-    // تغيير نوع الفلتر
+
     setFilter: (state, action) => {
       state.filter = action.payload;
     }
